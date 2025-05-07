920f181565aa4b86d5c1f39949248ccdebcd80b8
Matthias Kalms <mtkalms@gmail.com>
2025-05-07 14:48:48 -0700

    Add tagline

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,10 +1,12 @@
 interface SectionProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
   chapter: string,
+  tagline?: string,
   section?: string,
 }
 
 function Section({
   chapter,
+  tagline,
   section,
   children,
   ...props
@@ -12,18 +14,19 @@ function Section({
   return <section data-background-color="#1E1E3F" className="w-full h-full" {...props}>
     {section && 
       <div className="absolut inset-0 flex gap-4">
-        <span className="text-[#FAD000] font-bold">{chapter}</span>
-        <span className="text-[#FAD000] font-bold">|</span>
+        <span className="text-[var(--primary)] font-bold">{chapter}</span>
+        <span className="text-[var(--primary)] font-bold">|</span>
         <span>{section}</span>
       </div>
     }
     <div className="w-full h-full flex gap-4 items-center pb-30">
       {section ? <>{children}</> :
         <>
-          <div className="w-full h-full flex items-center justify-center text-[#FAD000] font-bold text-8xl">
-            {chapter}
+          <div className="w-full h-full flex flex-col gap-4 items-center justify-center">
+            <span className="text-[var(--primary)] font-bold text-8xl">{chapter}</span>
+            <span className="text-[var(--highlight)] font-semibold">{tagline}</span>
           </div>
-          <div className="w-full border-l-4 border-l-[#FAD000] text-left py-10 px-15">
+          <div className="w-full border-l-4 border-l-[var(--primary)] text-left py-20 px-15">
             {children}
           </div>
         </>
