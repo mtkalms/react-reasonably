f8010b570845e92d413bba85cd0374a290357aab
Matthias Kalms <mkalms@bccfe.ca>
2025-05-09 11:23:08 -0700

    Fix rendering multiple child nodes

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -16,12 +16,12 @@ function CodeBlock({
     const highlight: any = reveal?.deckRef.current?.getPlugin('highlight');
     code.current?.setAttribute('data-highlighted', "");
     highlight?.hljs.highlightElement(code.current);
-  }, [code, children])
+  }, [children])
 
   return <div className="w-full h-fit text-left shadow-xl rounded-xl border-3 border-[var(--contrast)]">
     <pre className="!m-0 p-5 !w-full !shadow-none">
       <code ref={code} data-noescape className="language-typescript overvlow-visible !max-h-none !bg-transparent text-[12pt] leading-[1.25] !font-mono" {...props}>
-      {children}
+      {Array.isArray(children) ? children.join("") : children}
       </code>
     </pre>
   </div>
