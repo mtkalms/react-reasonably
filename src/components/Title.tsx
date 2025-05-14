8bea96627ec1e39dc63e57865ed1a87b42bd57b0
Matthias Kalms <mtkalms@gmail.com>
2025-05-14 09:36:23 -0700

    Add title background

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -14,8 +14,14 @@ function Title({ title, subtitle, ...props }: TitleProps) {
       className="h-full w-full"
       {...props}
     >
-      <div className="flex h-full w-full flex-col items-center justify-center gap-4">
-        <span className="text-9xl font-semibold text-(--primary)">{title}</span>
+      <div className="absolute -inset-70 -z-20 bg-[url(./react.svg)] bg-size-[40%] bg-bottom-right bg-no-repeat opacity-5 blur-xs drop-shadow-2xl/80" />
+      <div className="absolute -inset-50 -z-20 bg-[url(./react.svg)] bg-size-[40%] bg-top-left bg-no-repeat opacity-5 blur-sm drop-shadow-2xl/80" />
+      <div className="absolute inset-70 top-5 right-80 -z-20 bg-[url(./react.svg)] bg-size-[30%] bg-top-right bg-no-repeat opacity-6 blur-md drop-shadow-xl/100" />
+      <div className="absolute inset-50 bottom-0 left-100 -z-20 bg-[url(./react.svg)] bg-size-[30%] bg-bottom-left bg-no-repeat opacity-6 blur-md drop-shadow-lg/100" />
+      <div className="absoolute z-20 flex h-full w-full flex-col items-center justify-center gap-4">
+        <span className="-mb-5 text-9xl font-semibold text-(--primary)">
+          {title}
+        </span>
         <span className="text-(--highlight)">{subtitle}</span>
       </div>
     </section>
