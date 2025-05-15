69ad8a726f1b6bb62eca6d8046ba1b6fe39486dd
Matthias Kalms <mtkalms@gmail.com>
2025-05-15 00:08:31 -0700

    Add types of components

--- src/chapters/custom-components/index.tsx
+++ src/chapters/custom-components/index.tsx
@@ -11,13 +11,44 @@ function CustomComponentsChapter() {
       <Section chapter={CHAPTER} tagline={TAGLINE} />
       <Section chapter={CHAPTER} tagline={TAGLINE}>
         <ul>
+          <li>Types of Components</li>
           <li>Composition</li>
           <li>Controlled Components</li>
           <li>Uncontrolled Components</li>
           <li>Higher Order Components (HOC)</li>
         </ul>
       </Section>
-      <Section chapter={CHAPTER} section="Composition" >
+      <Section chapter={CHAPTER} section="Types of Components">
+        <TextBlock>
+          <p>
+            <b>Deprecated</b>
+          </p>
+          <ul>
+            <li>CreateClass Factory</li>
+            <li>Mixin Pattern</li>
+          </ul>
+          <p>
+            <b>Not recomended anymore</b>
+          </p>
+          <ul>
+            <li>Class Component</li>
+            <li>Higher-Order Component</li>
+            <li>Render Props Pattern</li>
+          </ul>
+          <p>
+            <b>Current</b>
+          </p>
+          <ul>
+            <li>Functional Component</li>
+            <ul>
+              <li>Uncontrolled Component</li>
+              <li>Controlled Component</li>
+            </ul>
+            <li>Server Component</li>
+          </ul>
+        </TextBlock>
+      </Section>
+      <Section chapter={CHAPTER} section="Composition">
         <TextBlock>
           <Message.Container>
             <Message type="antipattern">
